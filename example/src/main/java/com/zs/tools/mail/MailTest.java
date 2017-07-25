000b5ea359587814d9bca137e98bf2041ce8d2bb
张顺 <1217360619@qq.com>
2017-07-25 16:58:08 +0800

    共两点。本地接口测试、邮件日志系统： 1、新增本地接口测试页面 2、新增邮件日志系统
    Signed-off-by: 张顺 <1217360619@qq.com>

---
+++ example/src/main/java/com/zs/tools/mail/MailTest.java
@@ -0,0 +1,100 @@
+package com.zs.tools.mail;
+
+import java.util.Properties;
+import javax.mail.Authenticator;
+import javax.mail.Message.RecipientType;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import org.apache.log4j.Logger;
+
+public class MailTest {
+
+	// 配置发送邮件的环境属性
+	public final static Properties props = new Properties();
+	
+	/**
+     * 可用的属性： mail.store.protocol / mail.transport.protocol / mail.host /
+     * mail.user / mail.from
+     */
+    // 表示SMTP发送邮件，需要进行身份验证
+	static{
+		/*
+		props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.qq.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.user", "3554832577@qq.com");// 发件人的账号
+        props.put("mail.password", "[抱歉不能告诉你]");// 访问SMTP服务时需要提供的密码
+        */
+        
+		props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.exmail.qq.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.user", "itrobot@szexpress.com.cn");// 发件人的账号
+        props.put("mail.password", "[抱歉不能告诉你]");// 访问SMTP服务时需要提供的密码	
+        
+	}
+	
+	
+	public static final String IT_ROBOT="itrobot@szexpress.com.cn";
+	
+	
+	private static Logger logger = Logger.getLogger(MailTest.class);
+	
+	/**张顺
+	 * <br>2016年9月2日11:35:15
+	 * <br>发邮件
+	 * @param toAddress
+	 * @param ccAddress
+	 * @param mailContent
+	 * @param title
+	 * @return
+	 * @throws MessagingException
+	 */
+	public static String outputMail(MailModel mail) throws MessagingException {
+        // 构建授权信息，用于进行SMTP进行身份验证
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                // 用户名、密码
+                String userName = props.getProperty("mail.user");
+                String password = props.getProperty("mail.password");
+                return new PasswordAuthentication(userName, password);
+            }
+        };
+        // 使用环境属性和授权信息，创建邮件会话
+        Session mailSession = Session.getInstance(props, authenticator);
+        // 创建邮件消息
+        MimeMessage message = new MimeMessage(mailSession);
+        // 设置发件人
+        InternetAddress form = new InternetAddress(props.getProperty("mail.user"));
+        message.setFrom(form);
+        // 设置收件人
+        InternetAddress to = new InternetAddress(mail.getToAddress());
+        message.setRecipient(RecipientType.TO, to);
+
+        // 设置抄送
+        InternetAddress cc = new InternetAddress(mail.getCcAddress());
+        message.setRecipient(RecipientType.CC, cc);
+
+        // 设置密送，其他的收件人不能看到密送的邮件地址
+        InternetAddress bcc = new InternetAddress(mail.getCcAddress());
+        message.setRecipient(RecipientType.CC, bcc);
+        // 设置邮件标题
+        message.setSubject(mail.getTitle());
+
+        // 设置邮件的内容体
+        message.setContent(mail.getMailContent(), "text/html;charset=UTF-8");
+        // 发送邮件
+        Transport.send(message);
+        
+        //返回一个值，用于判断邮件状态，暂时还没设计，留着以后用
+		return null;  
+        
+	}
+
+	
+}
---
+++ example/src/main/webapp/WEB-INF/jsp/system/test.jsp
@@ -0,0 +1,68 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%
+String path = request.getContextPath();
+String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
+%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>本地接口测试工具</title>
+</head>
+<body>
+<jsp:include page="/WEB-INF/jsp/part/common.jsp"/>
+<script type="text/javascript">
+	function addBody(){
+		var par=$('#http_body');
+		var first=$('#div1');
+		var len=par.children().length;
+		first.after("<div id='div"+(len+1)+"'>Key:<input type=\"text\"/>Value:<input type=\"text\"/><input type=\"button\" value=\"删除参数\" onclick=\"removeChildren('div"+(len+1)+"')\"/></div>");  
+	}
+	function removeChildren(id){
+		$("#"+id).remove();
+	}
+	function sendHttpPost(){
+		var arr = new Object(); 						
+		$('#http_body').children().each(function(i,n){
+			var obj = $(n);
+			var key=obj.children().eq(0).val();
+			var value=obj.children().eq(1).val();
+			arr[key+""]=value+"";
+	    });
+        console.log(JSON.stringify(arr));
+        var urltmp=$('#urltmp').val();//后面的url
+		$.post(
+			"<%=path %>/"+urltmp,
+			arr,
+			function(data){
+				console.log(data);
+				$("#result").append("<P>"+JSON.stringify(data)+"</p>");
+			}
+		);
+	}
+	function clearResult(){
+		$("#result").html("");
+	}
+</script>
+
+本地测试接口<br/>
+URL：只用写controller的url（开头不带/）<input id="urltmp" type="text"/>(写后面的部分,开头不要带/)
+<input type="button" value="添加参数" onclick="addBody()" style="background-color: rgba(230, 255, 0, 0.33);"/>
+<br>
+<div id="http_body" style="border:1px solid rgba(4, 253, 4, 0.43);padding: 10px;">
+	<div id="div1">Key:<input type="text"/>Value:<input type="text"/></div>
+</div>
+<br>
+<input type="button" value="发送请求" onclick="sendHttpPost()"/>请在开发者模式的console中查看结果
+
+<hr>
+<input type="button" value="清除结果" onclick="clearResult()"/>
+<div id="result">
+
+</div>
+
+
+</body>
+</html>
